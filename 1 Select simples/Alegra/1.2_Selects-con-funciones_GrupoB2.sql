9882302f2f76a47991b01b33855f0d2600c60d4e
Bryan2411 <bryan.naranjo@idesie.com>
2023-02-21 18:03:07 +0100

    Update 1.2_Selects-con-funciones_GrupoB2.sql

--- 1 Select simples/Alegra/1.2_Selects-con-funciones_GrupoB2.sql
+++ 1 Select simples/Alegra/1.2_Selects-con-funciones_GrupoB2.sql
@@ -5,7 +5,7 @@
 Mostrar la fecha actual de la siguiente forma:
 Fecha actual
 ------------------------------
-Sábado, 11 de febrero de 2017. 16:06:06
+Sábado, 11 de febrero de 2027. 16:06:06
 
 El día en palabras con la primera letra en mayúsculas, seguida de una coma, el día en números,
 la palabra "de", el mes en minúscula en palabras, la palabra "de", el año en cuatro dígitos
@@ -14,56 +14,112 @@ Y de etiqueta del campo "Fecha actual".
 */
 
 /* 2
-Día en palabras en el cual naciste
+Día en palabras de cuando se instalaron los componentes
+del facility 1
 */
 
 /* 3
-La suma de salarios, cuál es el mínimo, el máximo y la media de salario
+De los espacios, obtener la suma de áreas, cuál es el mínimo, el máximo y la media de áreas
+del floorid 1. Redondeado a dos dígitos.
 */
 
 /* 4
-Cuántos empleados hay, cuántos tienen salario y cuántos tienen comisión.
+Listar el número de componentes que tienen indicado el espacio y el número de componentes total.
+del facility 1
 */
 
 /* 5
-Por un lado la media entre la media de salarios y el mínimo salario
-Y por otro lado, la media entre la media de salarios y el máximo salario
+Mostrar tres medias que llamaremos:
+-Media a la media del área bruta
+-MediaBaja la media entre el área media y el área mínima
+-MediaAlta la media entre el área media y el área máxima
+de los espacios del floorid 1
 Solo la parte entera, sin decimales ni redondeo.
 */
 
 /* 6
-Listar el número de departamento y el máximo salario en cada uno de ellos.
+Cuántos componentes hay, cuántos tienen fecha inicio de garantia, cuántos tienen espacio, y en cuántos espacios hay componentes
+en el facility 1.
 */
 
 /* 7
-Mostrar los nombres de los empleados que se repiten indicando cuántos hay del mismo
-en orden descendente.
+Mostrar cuántos espacios tienen el texto 'Aula' en el nombre
+del facility 1.
 */
 
 /* 8
-Mostrar en una fila cuántos empleados son jefes de departamento
-y en otra fila cuántos son jefes de otros empleados.
+Mostrar el porcentaje de componentes que tienen fecha de inicio de garantía
+del facility 1.
 */
 
 /* 9
-Listar nombre, apellido de los empleados que les coindice a la vez
-la primera letra de su nombre y el apellido
+Listar las cuatro primeras letras del nombre de los espacios sin repetir
+del facility 1. 
+En orden ascendente.
+Ejemplo:
+Aula
+Area
+Aseo
+Pasi
+Pati
+Serv
 */
 
 /* 10
-Número de empleados dados de alta por día
-ordenados descendentemente por la fecha
+Número de componentes por fecha de instalación del facility 1
+ordenados descendentemente por la fecha de instalación
+Ejemplo:
+Fecha   Componentes
+-------------------
+2021-03-23 34
+2021-03-03 232
 */
 
 /* 11
-Un listado por año de la media de salarios
-de los empleados que ingresaron ese año
-ordenados de forma descendente por año
+Un listado por año del número de componentes instalados del facility 1
+ordenados descendentemente por año.
+Ejemplo
+Año Componentes
+---------------
+2021 344
+2020 2938
 */
 
 /* 12
-Nombre del día en el que más empleados
-se han dado de alta
+Nombre del día de instalación y número de componentes del facility 1.
+ordenado de lunes a domingo
+Ejemplo:
+Día         Componentes
+-----------------------
+Lunes    	503
+Martes   	471
+Miércoles	478
+Jueves   	478
+Viernes  	468
+Sábado   	404
+Domingo  	431
 */
 
+/*13
+Mostrar en base a los cuatro primeros caracteres del nombre cuántos espacios hay
+del floorid 1 ordenados ascendentemente por el nombre.
+Ejemplo.
+Aula 23
+Aseo 12
+Pasi 4
+*/
+
+/*14
+Cuántos componentes de instalaron un Jueves
+en el facilityid 1
+*/
+
+/*15
+Listar el id de planta concatenado con un guión
+seguido del id de espacio concatenado con un guión
+y seguido del nombre del espacio.
+el id del espacio debe tener una longitud de 3 caracteres
+Ej. 3-004-Nombre
+*/
+ 
 ------------------------------------------------------------------------------------------------
