496f1602d37b7c878a66afc62ca95c53d0e3a9a8
Celia Espadas Alonso-Barajas <celia.espadas@idesie.com>
2023-02-27 19:24:06 +0100

    JOIN

---
+++ 2 JOINS/HR/3.1_DML_GrupoX.sql
@@ -0,0 +1,41 @@
+------------------------------------------------------------------------------------------------
+-- DML
+------------------------------------------------------------------------------------------------
+/* 1
+Insertar un componente en el facility 1 
+con nombre «Grifo | Grifo | 030303» 
+descripcion «test insert»
+número de serie «666333-eeefff»
+fecha de instalación «2021-12-12»
+inicio de garantia «2021-11-11»
+código de activo «666000»
+id del creador «3»
+id del espacio «7»
+id de tipo «78»
+guid «666000»
+*/
+
+/*
+Comprobar que se ven los datos insertados de forma conjunta con una JOIN
+y no de forma independiente. Con el fin de comprobar las relaciones.
+Mostrar todos los datos indicados en el punto anterior 
+y además el nombre del espacio, nombre de la planta, nombre del tipo de componente
+*/
+
+/* 2
+Eliminar el componente creado.
+*/
+
+/* 3
+Colocar como código de barras los 6 últimos caracteres del GUID 
+a todo componente de la planta 1 y 2 del facility 1.
+*/
+
+/* 4
+Modificar la fecha de garantia para que sea igual a la fecha de instalación
+para todo componente que sea un grifo o lavabo del facility 1.
+*/
+
+/* 5
+Anonimizar los datos personales: nombre, apellido, email, teléfono de los contactos
+*/
