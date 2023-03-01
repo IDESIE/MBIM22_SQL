7f3ef30ce5668158f2d6d05d8af63321b27777d2
tpovedano <124092196+tpovedano@users.noreply.github.com>
2023-03-01 08:59:52 +0100

    Ejercicio

--- 3 DML/Alegra/3.1_DML_GrupoA2.sql
+++ 3 DML/Alegra/3.1_DML_GrupoA2.sql
@@ -15,6 +15,9 @@ id de tipo «78»
 guid «666000»
 */
 
+insert into cb_components(componentid,facilityid,name,description,serialnumber,assetidentifier,createdby,spaceid,typeid,externalidentifier,installatedon,warrantystarton)
+values(3,1,'Grifo | Grifo | 030303','test insert','666333-eeefff','666000','3',1,1,'eee-fff',to_date('2021-12-12','yyyy-mm-dd'),to_date('2021-11-11','yyyy-mm-dd'));
+
 /*
 Comprobar que se ven los datos insertados de forma conjunta con una JOIN
 y no de forma independiente. Con el fin de comprobar las relaciones.
@@ -22,9 +25,19 @@ Mostrar todos los datos indicados en el punto anterior
 y además el nombre del espacio, nombre de la planta, nombre del tipo de componente
 */
 
+select
+    cb_spaces.name,
+    cb_floors.name,
+    cb_components.name
+from cb_spaces
+join cb_components on cb_components.spaceid = cb_spaces.spaceid
+join cb_floors on cb_floors.floorid = cb_spaces.spaceid;
+
 /* 2
 Eliminar el componente creado.
 */
+
+rollback
 
 /* 3
 Colocar como código de barras los 6 últimos caracteres del GUID 
