bcac298bd7661da99385f7d994e659c5c4099f61
Lucía Zúñiga <lucia.zuniga@idesie.com>
2023-03-01 17:24:17 +0100

    Update 3.1_DML_GrupoA1.sql
    Review

--- 3 DML/Alegra/3.1_DML_GrupoA1.sql
+++ 3 DML/Alegra/3.1_DML_GrupoA1.sql
@@ -73,8 +73,8 @@ Modificar la fecha de garantia para que sea igual a la fecha de instalación
 para todo componente que sea un grifo o lavabo del facility 1.
 */
 
-Alter Table cb_components
-Modify warrantystarton = installatedon
+Update cb_components
+Set warrantystarton = installatedon
 where facilityid=1
 and (lower(name) is like 'grifo'
     or lower(name) is like 'lavado');
